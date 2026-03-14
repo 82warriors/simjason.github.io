7f0db45dd7a5e1d82680dd83b73dcbde4c618dd4
82warriors <simjs1982@gmail.com>
2026-03-14 16:15:09 +0800

    Add Streamlit title and introductory text

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.title("Jason Sim")
+
+st.write("Digital Systems Builder | Educator")
+
+st.write("""
+Welcome to my personal site where I share
+projects, ideas and systems I build.
+""")
