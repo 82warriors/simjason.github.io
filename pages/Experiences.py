1a117a72815962a3bcf00da14dc6157cf3572021
82warriors <simjs1982@gmail.com>
2026-03-15 17:14:13 +0800

    Update Experiences.py

--- pages/Experiences.py
+++ pages/Experiences.py
@@ -8,11 +8,24 @@ navbar()
 st.title("Experiences")
 
 st.subheader("Infocomm Technology Manager")
+
 st.write("Ministry of Education — 2018 to Present")
 
 st.write("""
 • Led ICT digital initiatives  
-• Built automation workflows  
-• Developed operational dashboards  
+• Developed automation solutions  
+• Built analytics dashboards  
 • Managed ICT infrastructure
+""")
+
+st.divider()
+
+st.subheader("Digital Trainer")
+
+st.write("Eduvantage Pte Ltd — 2014 to 2018")
+
+st.write("""
+• Managed recruitment processes  
+• Maintained company website  
+• Delivered staff training
 """)
