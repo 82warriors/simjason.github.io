b52b2c6afb04bee29f70ffb90dca0b69d9b5e550
82warriors <simjs1982@gmail.com>
2026-03-15 17:13:06 +0800

    Update nav.py

--- nav.py
+++ nav.py
@@ -2,47 +2,42 @@ import streamlit as st
 
 def navbar():
 
+    # hide sidebar
     st.markdown("""
     <style>
+    [data-testid="stSidebar"] {display:none;}
 
-    [data-testid="stSidebar"] {display:none}
-
-    .nav{
+    .nav-container {
         display:flex;
         justify-content:space-between;
         align-items:center;
-        padding:20px 60px;
-        border-bottom:1px solid #eee;
+        padding:10px 0px 30px 0px;
     }
 
-    .logo{
-        font-size:20px;
+    .logo {
+        font-size:22px;
         font-weight:700;
     }
 
-    .menu{
-        display:flex;
-        gap:40px;
-    }
-
-    .menu a{
-        text-decoration:none;
-        color:#111;
-        font-weight:500;
-    }
-
     </style>
-
-    <div class="nav">
-
-        <div class="logo">Jason Sim</div>
-
-        <div class="menu">
-            <a href="/">Home</a>
-            <a href="/AboutMe">About</a>
-            <a href="/Experiences">Experiences</a>
-            <a href="/Projects">Projects</a>
-        </div>
-
-    </div>
     """, unsafe_allow_html=True)
+
+    col1, col2 = st.columns([6,4])
+
+    with col1:
+        st.markdown('<div class="logo">Jason Sim</div>', unsafe_allow_html=True)
+
+    with col2:
+        c1, c2, c3, c4 = st.columns(4)
+
+        with c1:
+            st.page_link("Home.py", label="Home")
+
+        with c2:
+            st.page_link("pages/AboutMe.py", label="About")
+
+        with c3:
+            st.page_link("pages/Experiences.py", label="Experiences")
+
+        with c4:
+            st.page_link("pages/Projects.py", label="Projects")
