3a179dfe34a81d71eb596075775c7dbb14957f87
82warriors <simjs1982@gmail.com>
2026-03-15 16:29:30 +0800

    Update nav.py

--- nav.py
+++ nav.py
@@ -7,35 +7,59 @@ def navbar():
 
     [data-testid="stSidebar"] {display:none}
 
-    .nav-container{
+    .nav{
         display:flex;
-        justify-content:flex-end;
-        gap:40px;
-        padding-top:20px;
-        padding-bottom:20px;
-        font-size:18px;
+        justify-content:space-between;
+        align-items:center;
+        padding:20px 40px;
+        border-bottom:1px solid #eee;
+    }
+
+    .logo{
+        font-size:20px;
+        font-weight:700;
+    }
+
+    .menu{
+        display:flex;
+        gap:20px;
+    }
+
+    .menu button{
+        background:none;
+        border:none;
+        font-size:16px;
         font-weight:500;
+        cursor:pointer;
     }
 
-    .nav-link{
-        text-decoration:none;
-        color:#111;
-    }
-
-    .nav-link:hover{
-        color:#4cc9f0;
+    .menu button:hover{
+        color:#6366f1;
     }
 
     </style>
     """, unsafe_allow_html=True)
 
-    st.markdown("""
-    <div class="nav-container">
+    col1, col2 = st.columns([6,4])
 
-    <a class="nav-link" href="/">Home</a>
-    <a class="nav-link" href="/AboutMe">About</a>
-    <a class="nav-link" href="/Experiences">Experiences</a>
-    <a class="nav-link" href="/Projects">Projects</a>
+    with col1:
+        st.markdown('<div class="logo">Jason Sim</div>', unsafe_allow_html=True)
 
-    </div>
-    """, unsafe_allow_html=True)
+    with col2:
+        colA, colB, colC, colD = st.columns(4)
+
+        with colA:
+            if st.button("Home"):
+                st.switch_page("Home.py")
+
+        with colB:
+            if st.button("About"):
+                st.switch_page("pages/AboutMe.py")
+
+        with colC:
+            if st.button("Experiences"):
+                st.switch_page("pages/Experiences.py")
+
+        with colD:
+            if st.button("Projects"):
+                st.switch_page("pages/Projects.py")
