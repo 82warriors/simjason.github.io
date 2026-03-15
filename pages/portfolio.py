c708f68e9877a3b7474b615ab9a02a685291f76f
82warriors <simjs1982@gmail.com>
2026-03-15 10:13:44 +0800

    Add portfolio page with career timeline

---
+++ pages/portfolio.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.title("Career Timeline")
+
+st.markdown("""
+### 2018 – Present  
+**Infocomm Technology Manager**  
+Ministry of Education
+
+- Lead ICT initiatives
+- Develop automation systems
+- Build analytics dashboards
+
+---
+
+### 2014 – 2018  
+**Digital Trainer & Executive**  
+Eduvantage Pte Ltd
+
+- Staff training
+- Website management
+- Digital outreach
+
+---
+
+### Early Career  
+Engineering and technology background
+supporting digital transformation initiatives.
+""")
